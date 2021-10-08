a523f3ef867cd97b44aac62923effd0bb0d56e0e
Shaikh Intekhab <intekhabshaikh19@gmail.com>
2021-10-08 16:13:57 +0530

    Code Logic implemented

--- bmi_calc.py
+++ bmi_calc.py
@@ -1,1 +1,27 @@
-bmi
+
+
+def get_category_and_risk(bmi):
+    category_type = 'Underweight'
+    risk = "Malnutrition risk"
+    if bmi>=18.5 and bmi<25.0:
+        category_type = "Normal weight"
+        risk = "Low risk"
+    elif bmi>=25.0 and bmi<30.0:
+        category_type = "Overweight"
+        risk = "Enhanced risk"
+    elif bmi>=30.0 and bmi<40.0:
+        category_type = "Moderately obese"
+        risk = "Medium risk"
+    elif bmi>=30.0 and bmi<40.0:
+        category_type = "Severely obese"
+        risk = "High risk"
+    elif bmi>=40:
+        category_type = "Very severely obese"
+        risk = "Very high risk"
+    return category_type, risk
+
+
+def get_bmi_data(mass, height_cm):
+    bmi =  mass/(height_cm/100)
+    category, risk = get_category_and_risk(bmi)
+    return  bmi, category, risk
--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-main.py
+from bmi_calc import get_bmi_data
+from config import DATA, OVERWEIGHT_CATEGORY
+
+if __name__ == "__main__":
+    
+    bmi_result_data = DATA
+
+    for index, data in enumerate(DATA):
+        bmi_result_data[index]["bmi"],bmi_result_data[index]["category"], bmi_result_data[index]["health_risk"] = \
+            get_bmi_data(data["WeightKg"], data["HeightCm"])
+
+    overweight_count = len([data for data in bmi_result_data if data["category"] in OVERWEIGHT_CATEGORY])
+    print(bmi_result_data)
+    print(f"Total number of overweight people are: {str(overweight_count)}")
+
+    
